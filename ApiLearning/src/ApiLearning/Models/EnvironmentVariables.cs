16cab949c88d220b4f068e0d535cc25c7ada2215
Stewart Cole <coleste001@gmail.com>
2016-10-05 15:14:06 -0700

    Initial commit

---
+++ ApiLearning/src/ApiLearning/Models/EnvironmentVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLearning.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string AccountSid = "AC1b74c347f5727635e0cf72ca5318e445";
+        public static string AuthToken = "4762cfbf7f4f9caf55f0906873a3480a";
+    }
+}
---
+++ ApiLearning/src/ApiLearning/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ApiLearning.Models;
+
+namespace ApiLearning
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
